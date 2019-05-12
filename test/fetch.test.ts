631c512f826dc53c85725caa454ae30b6a16acfd
haochengz <haochengz@outlook.com>
2019-05-13 02:02:10 +0800

    test(fetch.test.ts): fix the entry file test

--- test/fetch.test.ts
+++ test/fetch.test.ts
@@ -1,4 +1,4 @@
-import DummyClass from '../src/fetch'
+import fetch from '../src/fetch'
 
 /**
  * Dummy test
@@ -8,7 +8,7 @@ describe('Dummy test', () => {
     expect(true).toBeTruthy()
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('should be a function', () => {
+    expect(typeof fetch).toEqual('function')
   })
 })
